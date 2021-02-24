f9a682b4493e4b8d0d74301c317371f4965ab3fd
SebastianWarvsten <sebastian.warvsten@iths.se>
2021-02-24 17:29:56 +0100

    Login page UI created and outlets bound

--- WestcostEducationLab/ViewController.swift
+++ WestcostEducationLab/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    @IBAction func createAccountButton(_ sender: UIButton) {
+    }
+    
 }
 
