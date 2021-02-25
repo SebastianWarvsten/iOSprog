ff3e30c9003f3e0ff56725ccb983090727821ee0
SebastianWarvsten <sebastian.warvsten@iths.se>
2021-02-25 16:13:37 +0100

    Added alert boxes for account registration & checks for the registration form

--- WestcostEducationLab/ViewControllers/RegisterViewController.swift
+++ WestcostEducationLab/ViewControllers/RegisterViewController.swift
@@ -11,7 +11,7 @@ class RegisterViewController: UIViewController {
     
     @IBOutlet weak var firstNameTextfield: UITextField!
     @IBOutlet weak var lastNameTextfield: UITextField!
-    @IBOutlet weak var emainTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var repeatPasswordTextfield: UITextField!
     
@@ -22,5 +22,36 @@ class RegisterViewController: UIViewController {
         view.addGestureRecognizer(tapGesture)
     }
     @IBAction func registerButton(_ sender: UIButton) {
+        let firstName = firstNameTextfield.text
+        let lastName = lastNameTextfield.text
+        let email = emailTextField.text
+        let password = passwordTextfield.text
+        let repeatPassword = repeatPasswordTextfield.text
+        
+        if firstName!.isEmpty || lastName!.isEmpty || email!.isEmpty || password!.isEmpty || repeatPassword!.isEmpty{
+            displayAlertMessage(message: "All fields need to be filled in")
+            return
+        }
+        if password != repeatPassword {
+            displayAlertMessage(message: "The passwords do not match")
+            return
+        }
+        
+        //Store data in core data
+        
+        //save info in core data and dismiss the popover
+        //...also show message that the account has been created
+        let myAlert = UIAlertController(title: "Congratulations", message: "Registration successfull!", preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){ action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        myAlert.addAction(okAction)
+        self.present(myAlert, animated: true, completion: nil)
+    }
+    func displayAlertMessage(message: String) {
+        let myAlert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        myAlert.addAction(okAction)
+        self.present(myAlert, animated: true, completion: nil)
     }
 }
