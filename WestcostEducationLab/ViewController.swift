98a5f923a30139959b6719c95c7319ba7d59acf1
SebastianWarvsten <sebastian.warvsten@iths.se>
2021-02-25 10:44:02 +0100

    textfield keyboard types set & tap gesture to remove keyboard implemented

--- WestcostEducationLab/ViewController.swift
+++ WestcostEducationLab/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
     }
 
     @IBAction func loginButton(_ sender: UIButton) {
