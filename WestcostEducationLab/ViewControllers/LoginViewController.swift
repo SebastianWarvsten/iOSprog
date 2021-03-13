3db2cf2c00d2a50b14168856c4c1eb446bcda55b
SebastianWarvsten <sebastian.warvsten@iths.se>
2021-03-13 19:56:17 +0100

    Uppdated LoginVC with a uservalidater, removed load function and user variable

--- WestcostEducationLab/ViewControllers/LoginViewController.swift
+++ WestcostEducationLab/ViewControllers/LoginViewController.swift
@@ -13,36 +13,36 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     let context = (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
-    var user = UserModel(email: "", firstname: "", lastname: "", password: "", phonenumber: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tapGesture)
-        
     }
     
     @IBAction func loginButton(_ sender: UIButton) {
-        // hämtar från coreData
+        validateUser()
+    }
+    
+    func validateUser(){
         let userEmail = emailTextField.text
         let userPassword = passwordTextField.text
         
-        loadUser()
-        
-        if userEmail == user.email && userPassword == user.password {
-            print("Du är nu inloggad")
-            performSegue(withIdentifier: "loginSegue", sender: self)
-        } else {
-            print("Det fungerade inte att logga in")
-        }
-    }
-    func loadUser(){
-        
-        if let userFromCoreData = try? context.fetch(UserAccount.fetchRequest()) as? [UserAccount]{
-            for u in userFromCoreData{
-                user = UserModel(email: u.email ?? "", firstname: u.firstname ?? "", lastname: u.lastname ?? "", password: u.password ?? "", phonenumber: Int(u.phonenumber))
-                print("\(userFromCoreData)")
+        if let userCoreData = try? context.fetch(UserAccount.fetchRequest()) as? [UserAccount]{
+            if userCoreData.count > 0{
+                for u in userCoreData{
+                    _ = UserModel(email: u.email ?? "", firstname: u.firstname ?? "", lastname: u.lastname ?? "", password: u.password ?? "", phonenumber: Int(u.phonenumber))
+                    if u.email == userEmail && u.password == userPassword{
+                        print("Congratz you are in")
+                        performSegue(withIdentifier: "loginSegue", sender: self)
+                        return
+                    }else{
+                        print("Access denied!")
+                    }
+                }
+            }else{
+                print("No users registered on this device yet!")
             }
         }
     }
